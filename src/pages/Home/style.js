2066fa50cab249798062c0107291765d62e1bb56
Dorabang <psa1195@naver.com>
2023-04-01 22:38:31 +0900

    design: add Home css

---
+++ src/pages/Home/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const IntroSection = styled.section`
+  .title h2 {
+    font-family: 'Noto Serif KR', serif;
+    font-size: 3.6rem;
+    font-weight: 900;
+    font-style: italic;
+    color: #94a61a;
+  }
+`;
