980f4a11ef319c923034314e62b96181ed03f21b
Dorabang <psa1195@naver.com>
2023-04-08 21:42:08 +0900

    style: Change userToken acquired method

--- src/Router.js
+++ src/Router.js
@@ -1,37 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/Auth/SignIn';
 import SignUp from './pages/Auth/SignUp';
-import TodoBoard from './pages/Todo/TodoBoard';
+import TodoList from './pages/Todo';
 
 const Router = () => {
-  const [userObj, setUserObj] = useState(null);
-
   const SERVER_URI = 'https://www.pre-onboarding-selection-task.shop';
-
-  useEffect(() => {
-    setUserObj(localStorage.getItem('userToken'));
-  }, [userObj]);
+  const accessToken = localStorage.getItem('access_token');
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home userObj={userObj} />} />
+        <Route path='/' element={<Home accessToken={accessToken} />} />
 
         <Route
           path='/signin'
-          element={<SignIn userObj={userObj} SERVER_URI={SERVER_URI} />}
+          element={<SignIn accessToken={accessToken} SERVER_URI={SERVER_URI} />}
         />
 
         <Route
           path='/signup'
-          element={<SignUp userObj={userObj} SERVER_URI={SERVER_URI} />}
+          element={<SignUp accessToken={accessToken} SERVER_URI={SERVER_URI} />}
         />
 
         <Route
           path='/todo'
-          element={<TodoBoard userObj={userObj} SERVER_URI={SERVER_URI} />}
+          element={
+            <TodoList accessToken={accessToken} SERVER_URI={SERVER_URI} />
+          }
         />
       </Routes>
     </BrowserRouter>
