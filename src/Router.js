f036b76f16de64df9b8ab9d0b4710848ef1204f2
Dorabang <psa1195@naver.com>
2023-04-08 14:46:28 +0900

    chore: add SERVER_URI and props

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/Auth/SignIn';
 import SignUp from './pages/Auth/SignUp';
-import Todo from './pages/Todo/TodoBoard';
+import TodoBoard from './pages/Todo/TodoBoard';
 
 const Router = () => {
   const [userObj, setUserObj] = useState(null);
+
+  const SERVER_URI = 'https://www.pre-onboarding-selection-task.shop';
 
   useEffect(() => {
     setUserObj(localStorage.getItem('userToken'));
@@ -15,13 +17,22 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home userObj={userObj} />} />
 
-        <Route path='/signin' element={<SignIn />} />
+        <Route
+          path='/signin'
+          element={<SignIn userObj={userObj} SERVER_URI={SERVER_URI} />}
+        />
 
-        <Route path='/signup' element={<SignUp />} />
+        <Route
+          path='/signup'
+          element={<SignUp userObj={userObj} SERVER_URI={SERVER_URI} />}
+        />
 
-        <Route path='/todo' element={<Todo userObj={userObj} />} />
+        <Route
+          path='/todo'
+          element={<TodoBoard userObj={userObj} SERVER_URI={SERVER_URI} />}
+        />
       </Routes>
     </BrowserRouter>
   );
